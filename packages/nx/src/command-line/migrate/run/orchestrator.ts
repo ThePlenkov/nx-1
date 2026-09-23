nrwl/nx

fix(core): record the worker's death as the failure when an orchestrated migrate step is given up
A died step carries no outcome (markDied records the status only and a
re-arm rebuilds the step without one), so giving it up reached the
completion report and the minted issue with "no failure detail was
recorded". The died -> unresolved transition now records the death as
the step's failure, and the issue is minted from the transitioned step
instead of the pre-transition snapshot.

Also: the mintUnresolvedIssue doc no longer claims the migration id keeps
two steps from folding, the archive-shape comment sits above
newIssueArchiveRecord again, and the commitForStep contract names failed
adoption.

packages/nx/src/command-line/migrate/run/orchestrator.ts
@@ -884,7 +884,12 @@ export async function runOrchestratorReconcile(
       // authoritative, and undoing the transition for a lost detail file
       // would leave the agent re-issuing an action the run already took.
       if (stepAction === 'unresolved') {
-        const minted = mintUnresolvedIssue(rearmed, target);
+        // Minted from the transitioned step: a died one only gains its
+        // failure in the transition.
+        const minted = mintUnresolvedIssue(
+          rearmed,
+          rearmed.steps.find((s) => s.id === target.id)
+        );
         try {
           archiveIssues(dir, minted.application);
         } catch (e) {
@@ -1540,8 +1545,8 @@ async function stepActionSideEffects(
   }
 }
 
-// Commits the working tree left by a folded prompt outcome, an adopted death
-// or a given-up step, returning the ledger entry the caller persists together
+// Commits the working tree left by a folded prompt outcome, an adopted step
+// (failed or died) or a given-up step, returning the ledger entry the caller persists together
 // with the step transition (null when there was nothing to commit). The
 // worker's recorded-commit path classifies through the same
 // commitResultToLedgerEntry.

ThePlenkov/nx-1

packages/nx/src/command-line/migrate/run/orchestrator.ts
@@ -939,7 +939,12 @@ export async function runOrchestratorReconcile(
         // authoritative, and undoing the transition for a lost detail file
         // would leave the agent re-issuing an action the run already took.
         if (stepAction === 'unresolved') {
-          const minted = mintUnresolvedIssue(settled, target);
+          // Minted from the transitioned step: a died one only gains its
+          // failure in the transition.
+          const minted = mintUnresolvedIssue(
+            settled,
+            settled.steps.find((s) => s.id === target.id)
+          );
           try {
             archiveIssues(dir, minted.application);
           } catch (e) {
@@ -1636,8 +1641,8 @@ async function stepActionSideEffects(
   }
 }
 
-// Commits the working tree left by a folded prompt outcome, an adopted death
-// or a given-up step. The caller persists `entry` with the step transition
+// Commits the working tree left by a folded prompt outcome, an adopted step
+// (failed or died) or a given-up step. The caller persists `entry` with the step transition
 // unless `recorded` says a session's parent already did; null when nothing
 // to commit. A crash between the git commit and the state write leaves that
 // commit in history and out of the ledger; the refold then sees a clean tree.